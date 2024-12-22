2312783b156a219e1cd42bed3967579ef1eea684
Art <artchel06studios@yahoo.com>
2024-12-22 12:38:09 +0800

    add assets

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour {
+    public AudioClip clickSound; // The sound clip to play
+    private AudioSource audioSource; // The AudioSource to play the sound
+    public AudioClip hoverSound; // The sound clip to play
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    void Start () {
+
+        // Find or assign the AudioSource component
+        audioSource = FindFirstObjectByType<AudioSource>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -17,9 +22,27 @@ public class Menu : MonoBehaviour {
 
 	public void StartGame(){
 		SceneManager.LoadScene ("level1");
-	}
+        PlayClickSound();
+
+    }
 
 	public void Quit(){
-		Application.Quit ();
+        PlayClickSound();
+
+        Application.Quit ();
 	}
+    public void PlayClickSound()
+    {
+        if (audioSource != null && clickSound != null)
+        {
+            audioSource.PlayOneShot(clickSound);
+        }
+    }
+    public void PlayHoverSound()
+    {
+        if (audioSource != null && hoverSound != null)
+        {
+            audioSource.PlayOneShot(hoverSound);
+        }
+    }
 }
